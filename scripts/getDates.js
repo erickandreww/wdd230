8173281e8faa24b84f3fd4278db0d307976e26dc
erickandreww <erick.andrew.moura@gmail.com>
2023-09-14 21:12:59 -0300

    "Last modification added"

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,17 @@
 const h = new Date();
 document.getElementById('currentYear').textContent = h.getFullYear();
 
-let currentDate = new Date().toLocaleDateString();
-let hours = new Date().toJSON().slice(11, 19);
-document.getElementById('lastModified').textContent = `${currentDate} ${hours}`;
+let lastModif = new Date(document.lastModified);
+let currentDate = lastModif.toLocaleDateString();
+let hours = lastModif.getHours();
+let min = lastModif.getMinutes();
+let sec = lastModif.getSeconds();
+
+if (min < 10) {
+    min = "0"+min;
+}
+if (sec < 10) {
+    sec = "0"+sec;
+}
+
+document.getElementById('lastModified').textContent = `: ${currentDate} ${hours}:${min}:${sec} `;
