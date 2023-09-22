ab76910f3d1a7d28ea5db20d7270b74f6c749fac
erickandreww <erick.andrew.moura@gmail.com>
2023-09-22 00:59:44 -0300

    "developing dark mode"

---
+++ scripts/darkmode.js
@@ -0,0 +1,18 @@
+const modeButton = document.querySelector('#mode');
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+const nav = document.querySelector('nav');
+
+modeButton.addEventListener('click', () => {
+    if (modeButton.textContent.includes('☑️')) {
+        body.style.background = '#0D1B2A';
+        main.style.color = '#E0E1DD';
+        modeButton.textContent = '❎';
+        nav.style.background = '#415A77';
+    }
+    else {
+        body.style.background = '#9AE0F1';
+        main.style.color = '#0D1B2A';
+        modeButton.textContent = '☑️';
+    }
+});
